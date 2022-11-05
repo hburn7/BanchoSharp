201dd71052c3df9cf79c884b87b3847a23b52fac
hburn7 <hburnett777@gmail.com>
2022-11-05 15:12:33 -0400

    Example program now references an environment variable for the IRC username

--- BanchoSharpTest/Program.cs
+++ BanchoSharpTest/Program.cs
@@ -1,9 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BanchoSharp;
 
-using BanchoSharp;
-
-var client = new BanchoClient(new BanchoClientConfig(new IrcCredentials("Stage",
-	Environment.GetEnvironmentVariable("IRC_PASS")), LogLevel.Debug));
+var client = new BanchoClient(new BanchoClientConfig(new IrcCredentials(Environment.GetEnvironmentVariable("IRC_USERNAME"),
+	Environment.GetEnvironmentVariable("IRC_PASS")), LogLevel.Trace));
 
 client.OnAuthenticated += async () =>
 {
