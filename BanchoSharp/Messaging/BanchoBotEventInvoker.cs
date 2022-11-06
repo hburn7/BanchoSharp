c197fb6d5370850fd794763fc349eaf8ccda0c83
hburn7 <hburnett777@gmail.com>
2022-11-05 21:06:29 -0400

    Created BanchoBotEventInvoker class which is responsible for processing messages sent by BanchoBot and raising corresponding events.

---
+++ BanchoSharp/Messaging/BanchoBotEventInvoker.cs
@@ -0,0 +1,39 @@
+using BanchoSharp.Interfaces;
+using BanchoSharp.Multiplayer;
+
+namespace BanchoSharp.Messaging;
+
+public class BanchoBotEventInvoker : IBanchoBotEventInvoker, IBanchoBotEvents
+{
+	private readonly IBanchoClient _client;
+
+	public BanchoBotEventInvoker(IBanchoClient client) { _client = client; }
+
+	public void ProcessMessage(IPrivateIrcMessage msg)
+	{
+		if (!msg.IsBanchoBotMessage)
+		{
+			return;
+		}
+
+		string content = msg.Content;
+
+		if (IsMultiplayerLobbyCreation(content))
+		{
+			string lobbyId = content.Split("https://osu.ppy.sh/mp/")[1].Split()[0];
+			string name = content.Split(lobbyId)[1].Trim();
+
+			if (!long.TryParse(lobbyId, out long id))
+			{
+				Logger.Error($"Failed to parse lobby id {id}");
+				return;
+			}
+			
+			var mp = new MultiplayerLobby(_client, long.Parse(lobbyId), name);
+			OnTournamentLobbyCreated?.Invoke(mp);
+		}
+	}
+
+	private bool IsMultiplayerLobbyCreation(string content) => content.StartsWith("Created the tournament match");
+	public event Action<IMultiplayerLobby>? OnTournamentLobbyCreated;
+}
