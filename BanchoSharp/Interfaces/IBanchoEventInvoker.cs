5d738e3bd96f69986f90d8faf3ad72d30520acf6
hburn7 <hburnett777@gmail.com>
2022-11-05 21:44:24 -0400

    Added IBanchoBotEventInvoker

---
+++ BanchoSharp/Interfaces/IBanchoEventInvoker.cs
@@ -0,0 +1,10 @@
+namespace BanchoSharp.Interfaces;
+
+public interface IBanchoBotEventInvoker
+{
+	/// <summary>
+	/// Listens for events from BanchoBot. Fires any necessary client events if detected.
+	/// </summary>
+	/// <returns></returns>
+	public void ProcessMessage(IPrivateIrcMessage msg);
+}
