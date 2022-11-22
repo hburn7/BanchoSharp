6e30d784094a8fe8f74244c65e7798b6a29ee8c9
hburn7 <hburnett777@gmail.com>
2022-11-21 21:30:16 -0500

    Updated mods, tests

--- BanchoSharp/Multiplayer/MultiplayerMods.cs
+++ BanchoSharp/Multiplayer/MultiplayerMods.cs
@@ -1,77 +1,159 @@
-﻿namespace BanchoSharp.Multiplayer;
+﻿using System.Text;
 
-using System.Text;
+namespace BanchoSharp.Multiplayer;
 
 // It's important that these are named the same way Bancho names them in IRC, 
 // in order for them to get registered correctly.
 [Flags]
 public enum Mods
 {
-    None = 0,
-    Freemod = 1 << 0,
-    Easy = 1 << 1,
-    NoFail = 1 << 2,
-    HalfTime = 1 << 3,
-    HardRock = 1 << 4,
-    SuddenDeath = 1 << 5,
-    Perfect = 1 << 6,
-    DoubleTime = 1 << 7,
-    Nightcore = 1 << 8,
-    Hidden = 1 << 9,
-    FadeIn = 1 << 10,
-    Flashlight = 1 << 11,
-    Autopilot = 1 << 12,
-    Relax = 1 << 13,
-    SpunOut = 1 << 14
+	None = 0,
+	Freemod = 1 << 0,
+	Easy = 1 << 1,
+	NoFail = 1 << 2,
+	HalfTime = 1 << 3,
+	HardRock = 1 << 4,
+	SuddenDeath = 1 << 5,
+	Perfect = 1 << 6,
+	DoubleTime = 1 << 7,
+	Nightcore = 1 << 8,
+	Hidden = 1 << 9,
+	FadeIn = 1 << 10,
+	Flashlight = 1 << 11,
+	Autopilot = 1 << 12,
+	Relax = 1 << 13,
+	SpunOut = 1 << 14
 }
 
 public static class ModsExtensions
 {
-
     /// <summary>
-    /// Returns a string with a shorter format, example: "HDDT"
+    ///  Returns a string with a shorter format, example: "HDDT"
     /// </summary>
     public static string ToAbbreviatedForm(this Mods mods, bool showFreemod = true)
-    {
-        var ret = new StringBuilder(16);
-        
-        // Just going to keep appending to this string, to get everything
-        // in the "right" order. This might not be the cleanest solution, 
-        // but I can't think of anything better right now.
+	{
+		var ret = new StringBuilder(16);
 
-        if ((mods & Mods.Freemod) != 0 && showFreemod)
-            ret.Append("FM");        
-        if ((mods & Mods.Relax) != 0)
-            ret.Append("RX");
-        if ((mods & Mods.Autopilot) != 0)
-            ret.Append("AP");
-        if ((mods & Mods.SpunOut) != 0)
-            ret.Append("SO");
-        if ((mods & Mods.Easy) != 0)
-            ret.Append("EZ");
-        if ((mods & Mods.NoFail) != 0)
-            ret.Append("NF");
-        if ((mods & Mods.Hidden) != 0)
-            ret.Append("HD");
-        if ((mods & Mods.HalfTime) != 0)
-            ret.Append("HT");
-        if ((mods & Mods.DoubleTime) != 0)
-            ret.Append("DT");
-        if ((mods & Mods.Nightcore) != 0)
-            ret.Append("NC");
-        if ((mods & Mods.HardRock) != 0)
-            ret.Append("HR");
-        if ((mods & Mods.SuddenDeath) != 0)
-            ret.Append("SD");
-        if ((mods & Mods.Perfect) != 0)
-            ret.Append("PF");
-        if ((mods & Mods.Flashlight) != 0)
-            ret.Append("FL");
-        
-        if (ret.Length == 0)
-            ret.Append("None");
-        
-        return ret.ToString();
-    }
-    
+		// Just going to keep appending to this string, to get everything
+		// in the "right" order. This might not be the cleanest solution, 
+		// but I can't think of anything better right now.
+
+		if ((mods & Mods.Freemod) != 0 && showFreemod)
+		{
+			ret.Append("Freemod"); // This is to work with BanchoBot, not a 'real' mod like the rest.
+		}
+
+		if ((mods & Mods.Relax) != 0)
+		{
+			ret.Append("RX");
+		}
+
+		if ((mods & Mods.Autopilot) != 0)
+		{
+			ret.Append("AP");
+		}
+
+		if ((mods & Mods.SpunOut) != 0)
+		{
+			ret.Append("SO");
+		}
+
+		if ((mods & Mods.Easy) != 0)
+		{
+			ret.Append("EZ");
+		}
+
+		if ((mods & Mods.NoFail) != 0)
+		{
+			ret.Append("NF");
+		}
+
+		if ((mods & Mods.Hidden) != 0)
+		{
+			ret.Append("HD");
+		}
+
+		if ((mods & Mods.HalfTime) != 0)
+		{
+			ret.Append("HT");
+		}
+
+		if ((mods & Mods.DoubleTime) != 0)
+		{
+			ret.Append("DT");
+		}
+
+		if ((mods & Mods.Nightcore) != 0)
+		{
+			ret.Append("NC");
+		}
+
+		if ((mods & Mods.HardRock) != 0)
+		{
+			ret.Append("HR");
+		}
+
+		if ((mods & Mods.SuddenDeath) != 0)
+		{
+			ret.Append("SD");
+		}
+
+		if ((mods & Mods.Perfect) != 0)
+		{
+			ret.Append("PF");
+		}
+
+		if ((mods & Mods.Flashlight) != 0)
+		{
+			ret.Append("FL");
+		}
+
+		if (ret.Length == 0)
+		{
+			ret.Append("None");
+		}
+
+		return ret.ToString();
+	}
+}
+
+public static class ModsUtilities
+{
+	private static readonly Dictionary<string, Mods> _modsMap = new()
+	{
+		{ "None", Mods.None },
+		{ "NoFail", Mods.NoFail },
+		{ "Easy", Mods.Easy },
+		{ "HalfTime", Mods.HalfTime },
+		{ "Hidden", Mods.Hidden },
+		{ "FadeIn", Mods.FadeIn },
+		{ "HardRock", Mods.HardRock },
+		{ "Flashlight", Mods.Flashlight },
+		{ "DoubleTime", Mods.DoubleTime },
+		{ "Nightcore", Mods.Nightcore },
+		{ "SuddenDeath", Mods.SuddenDeath },
+		{ "SpunOut", Mods.SpunOut },
+		{ "Relax", Mods.Relax },
+		{ "Relax2", Mods.Autopilot },
+		{ "Freemod", Mods.Freemod }
+	};
+
+    /// <summary>
+    ///  Mods may be added to the end of a player report when doing !mp settings,
+    ///  or may be included in the `Active mods: xyz` portion of the !mp settings response.
+    ///  This method will convert mods into a "mods string". The intention is for
+    /// </summary>
+    /// <param name="mods"></param>
+    /// <returns></returns>
+    public static Mods GetModsFromString(params string[] mods)
+	{
+		Mods ret = 0;
+		foreach (string mod in mods)
+		{
+			var match = _modsMap[mod];
+			ret |= match;
+		}
+
+		return ret;
+	}
 }
--- BanchoSharpTests/MultiplayerTests.cs
+++ BanchoSharpTests/MultiplayerTests.cs
@@ -1,15 +1,67 @@
 using BanchoSharp.Interfaces;
 using BanchoSharp.Messaging;
 using BanchoSharp.Messaging.ChatMessages;
+using BanchoSharp.Multiplayer;
+using System.Text;
 
 namespace BanchoSharpTests;
 
 public class MultiplayerTests
 {
-	private IBanchoClient _client;
-	private IBanchoBotEventInvoker _invoker;
-	private IBanchoBotEvents _events;
+	// Commands
+	private const string _mpSettings =
+		"Room name: 3.00* - 5.00* | Auto Host Rotate, History: https://osu.ppy.sh/mp/104803682\nBeatmap: https://osu.ppy.sh/b/2216531 Sayuri - Koukai no Uta (TV Size)\nTeam mode: HeadToHead, Win condition: Score\nActive mods: Freemod\nPlayers: 7\nSlot 1  Not Ready https://osu.ppy.sh/u/22547049 Player 1        [Host]\nSlot 2  Not Ready https://osu.ppy.sh/u/14607058 Player 2        [Hidden, HardRock]\nSlot 3  Not Ready https://osu.ppy.sh/u/28851081 Player 3        \nSlot 4  Not Ready https://osu.ppy.sh/u/27831164 Player 4        \nSlot 6  Not Ready https://osu.ppy.sh/u/18028068 PlayerNameLarge1[Hidden, HardRock]\nSlot 7  Not Ready https://osu.ppy.sh/u/22940358 Player 6        \nSlot 8  Not Ready https://osu.ppy.sh/u/11796227 Player 7        ";
+
+	// private string _mpSettings(string lobbyName, int lobbyId, string curBeatmapName, int curBeatmapId,
+	// 	LobbyFormat curFormat, WinCondition winCondition, Mods activeMods, params MultiplayerPlayer[] players)
+	// {
+	// 	var builder = new StringBuilder($"Room name: {lobbyName}, History: https://osu.ppy.sh/mp/{lobbyId}")
+	// 	              .AppendLine($"Beatmap: https://osu.ppy.sh/b/{curBeatmapId} {curBeatmapName}")
+	// 	              .AppendLine($"Team mode: {curFormat}, Win condition: {winCondition}")
+	// 	              .AppendLine($"Active mods: {activeMods.ToAbbreviatedForm()}");
+	// }
 	
+	private const string _mpStart = "Started the match";
+	private const string _mpAbort = "Aborted the match";
+	
+	private IBanchoClient _client;
+	private IBanchoBotEvents _events;
+	private IBanchoBotEventInvoker _invoker;
+	private string _mpHost(string name) => $"Changed match host to {name}";
+	private string _mpMap(string id) => $"Changed beatmap to https://osu.ppy.sh/b/{id} EXAMPLE ARTIST - EXAMPLE TITLE";
+
+	// Events
+	private const string _matchStarted = "The match has started!";
+	private const string _matchFinished = "The match has finished!";
+	
+	private string _playerJoined(string name, int slot, string? team)
+	{
+		var builder = new StringBuilder($"{name} joined in slot {slot}");
+		if (team != null)
+		{
+			if (team.ToLower() is not "red" or "blue")
+			{
+				throw new InvalidOperationException($"Invalid team ({team}). Expected 'red' or 'blue'.");
+			}
+
+			builder.Append($" for team {team}");
+		}
+
+		builder.Append('.');
+		return builder.ToString();
+	}
+
+	private string _teamChanged(string name, int slot) => $"{name} moved to slot {slot}";
+	private string _playerLeft(string name) => $"{name} left the game.";
+
+	private string _matchFinishedPlayerStats(string name, int score, bool passed) => new StringBuilder($"{name} finished playing (Score: {score}, ")
+	                                                                                 .Append(passed ? "PASSED" : "FAILED")
+	                                                                                 .Append(").")
+	                                                                                 .ToString();
+
+	private string _hostChanged(string name) => $"{name} became the host.";
+	private string _beatmapChanged(string title, string diff, int id) => $"Beatmap changed to: {title} [{diff}] (https://osu.ppy.sh/b/{id})";
+
 	[SetUp]
 	public void Setup()
 	{
@@ -38,5 +90,11 @@ public class MultiplayerTests
 
 		_invoker.ProcessMessage(msg);
 		Assert.That(invoked, Is.True);
+	}
+
+	[Test]
+	public void TestMpSettings()
+	{
+		
 	}
 }
