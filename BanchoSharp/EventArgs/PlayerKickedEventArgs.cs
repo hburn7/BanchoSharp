94f732613d1d4342d72d264ded34c3c8f7fb72fe
hburn7 <hburnett777@gmail.com>
2022-12-06 22:46:05 -0500

    Added PlayerKickedEventArgs

---
+++ BanchoSharp/EventArgs/PlayerKickedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using BanchoSharp.Multiplayer;
+
+namespace BanchoSharp.EventArgs;
+
+public class PlayerKickedEventArgs : System.EventArgs
+{
+	public PlayerKickedEventArgs(MultiplayerPlayer player, DateTime kickTime)
+	{
+		Player = player;
+		KickTime = kickTime;
+	}
+
+	public MultiplayerPlayer Player { get; }
+	public DateTime KickTime { get; }
+}
