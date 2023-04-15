0e26bf6a0aa182b2549a3907058a5cb0472ecc2b
hburn7 <hburnett777@gmail.com>
2023-04-14 22:52:59 -0400

    PlayerDisconnectedEventArgs docs

--- BanchoSharp/EventArgs/PlayerDisconnectedEventArgs.cs
+++ BanchoSharp/EventArgs/PlayerDisconnectedEventArgs.cs
@@ -2,14 +2,29 @@ using BanchoSharp.Interfaces;
 
 namespace BanchoSharp.EventArgs;
 
+/// <summary>
+/// Represents the event arguments for a player disconnection.
+/// </summary>
 public class PlayerDisconnectedEventArgs : System.EventArgs
 {
-	public PlayerDisconnectedEventArgs(IMultiplayerPlayer player, DateTime disconnectedAt)
-	{
-		Player = player;
-		DisconnectedAt = disconnectedAt;
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerDisconnectedEventArgs"/> class.
+    /// </summary>
+    /// <param name="player">The <see cref="IMultiplayerPlayer"/> object related to the disconnection event.</param>
+    /// <param name="disconnectedAt">The <see cref="DateTime"/> value of when the disconnection occurred.</param>
+    public PlayerDisconnectedEventArgs(IMultiplayerPlayer player, DateTime disconnectedAt)
+    {
+        Player = player;
+        DisconnectedAt = disconnectedAt;
+    }
 
-	public IMultiplayerPlayer Player { get; }
-	public DateTime DisconnectedAt { get; }
+    /// <summary>
+    /// Gets the <see cref="IMultiplayerPlayer"/> object related to the disconnection event.
+    /// </summary>
+    public IMultiplayerPlayer Player { get; }
+
+    /// <summary>
+    /// Gets the <see cref="DateTime"/> value of when the disconnection occurred.
+    /// </summary>
+    public DateTime DisconnectedAt { get; }
 }
