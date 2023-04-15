79f6f94aec4ba3784085ba61947e22922a5d4e24
hburn7 <hburnett777@gmail.com>
2023-04-14 22:37:58 -0400

    More docs

--- BanchoSharp/EventArgs/PlayerBannedEventArgs.cs
+++ BanchoSharp/EventArgs/PlayerBannedEventArgs.cs
@@ -1,15 +1,31 @@
-﻿using BanchoSharp.Interfaces;
-
-namespace BanchoSharp.EventArgs;
-
-public class PlayerBannedEventArgs : System.EventArgs
+﻿
+using BanchoSharp.Interfaces;
+namespace BanchoSharp.EventArgs
 {
-	public PlayerBannedEventArgs(IMultiplayerPlayer player, DateTime banTime)
-	{
-		Player = player;
-		BanTime = banTime;
-	}
+    /// <summary>
+    /// Event arguments for when a player is banned from a lobby.
+    /// </summary>
+    public class PlayerBannedEventArgs : System.EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of the PlayerBannedEventArgs class.
+        /// </summary>
+        /// <param name="player">The player who was banned.</param>
+        /// <param name="banTime">The time the ban occurred.</param>
+        public PlayerBannedEventArgs(IMultiplayerPlayer player, DateTime banTime)
+        {
+            Player = player;
+            BanTime = banTime;
+        }
 
-	public IMultiplayerPlayer Player { get; }
-	public DateTime BanTime { get; }
+        /// <summary>
+        /// The player who was banned.
+        /// </summary>
+        public IMultiplayerPlayer Player { get; }
+
+        /// <summary>
+        /// The time the player was banned.
+        /// </summary>
+        public DateTime BanTime { get; }
+    }
 }
