56675bdacdade70fc7a15b55163d9eb7aa2700c0
hburn7 <hburnett777@gmail.com>
2023-04-14 22:47:40 -0400

    Docs for Logger

--- BanchoSharp/Logger.cs
+++ BanchoSharp/Logger.cs
@@ -1,50 +1,88 @@
 namespace BanchoSharp;
 
+/// <summary>
+///  Enum to hold log level flag.
+/// </summary>
 public enum LogLevel
 {
-	Trace,
-	Debug,
-	Info,
-	Warn,
-	Error,
-	None
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+    None
 }
 
+/// <summary>
+/// The Logger class provides a simple logging mechanism for the application 
+/// </summary>
 public static class Logger
 {
-	// Set by BanchoClientConfig
-	public static LogLevel LogLevel { get; set; }
-	public static void Trace(object value) => Log(LogLevel.Trace, value);
-	public static void Debug(object value) => Log(LogLevel.Debug, value);
-	public static void Info(object value) => Log(LogLevel.Info, value);
-	public static void Warn(object value) => Log(LogLevel.Warn, value);
-	public static void Error(object value) => Log(LogLevel.Error, value);
+    /// <summary>
+    /// Gets or sets the current log level used by the Logger.
+    /// </summary>
+    public static LogLevel LogLevel { get; set; }
 
-	private static void Log(LogLevel level, object value)
-	{
-		if (LogLevel == LogLevel.None)
-		{
-			return;
-		}
+    /// <summary>
+    /// Logs an object with the LogLevel Trace.
+    /// </summary>
+    /// <param name="value">The object to log.</param>
+    public static void Trace(object value) => Log(LogLevel.Trace, value);
 
-		if (level < LogLevel)
-		{
-			return;
-		}
+    /// <summary>
+    /// Logs an object with the LogLevel Debug.
+    /// </summary>
+    /// <param name="value">The object to log.</param>
+    public static void Debug(object value) => Log(LogLevel.Debug, value);
 
-		var defaultForeground = Console.ForegroundColor;
+    /// <summary>
+    /// Logs an object with the LogLevel Info.
+    /// </summary>
+    /// <param name="value">The object to log.</param>
+    public static void Info(object value) => Log(LogLevel.Info, value);
 
-		Console.ForegroundColor = level switch
-		{
-			LogLevel.Trace => ConsoleColor.DarkGray,
-			LogLevel.Debug => ConsoleColor.Gray,
-			LogLevel.Info => ConsoleColor.White,
-			LogLevel.Warn => ConsoleColor.Yellow,
-			LogLevel.Error => ConsoleColor.Red,
-			_ => Console.ForegroundColor
-		};
+    /// <summary>
+    /// Logs an object with the LogLevel Warn.
+    /// </summary>
+    /// <param name="value">The object to log.</param>
+    public static void Warn(object value) => Log(LogLevel.Warn, value);
 
-		Console.WriteLine($"{DateTime.Now:HH:mm:ss} [{level}]: {value}");
-		Console.ForegroundColor = defaultForeground;
-	}
+    /// <summary>
+    /// Logs an object with the LogLevel Error.
+    /// </summary>
+    /// <param name="value">The object to log.</param>
+    public static void Error(object value) => Log(LogLevel.Error, value);
+
+    /// <summary>
+    /// Logs an object with the specified log level.
+    /// </summary>
+    /// <param name="level">The LogLevel to use for this message.</param>
+    /// <param name="value">The object to log.</param>
+    private static void Log(LogLevel level, object value)
+    {
+        if (LogLevel == LogLevel.None)
+        {
+            return;
+        }
+
+        if (level < LogLevel)
+        {
+            return;
+        }
+
+        var defaultForeground = Console.ForegroundColor;
+
+        Console.ForegroundColor = level switch
+        {
+            LogLevel.Trace => ConsoleColor.DarkGray,
+            LogLevel.Debug => ConsoleColor.Gray,
+            LogLevel.Info => ConsoleColor.White,
+            LogLevel.Warn => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.Red,
+            _ => Console.ForegroundColor
+        };
+
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} [{level}]: {value}");
+        Console.ForegroundColor = defaultForeground;
+    }
 }
