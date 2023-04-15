c06072ad9c3174a741868eb989649425a50d288a
hburn7 <hburnett777@gmail.com>
2023-04-14 22:23:50 -0400

    Added docs to PlayerChangedteamEventArgs

--- BanchoSharp/EventArgs/PlayerChangedTeamEventArgs.cs
+++ BanchoSharp/EventArgs/PlayerChangedTeamEventArgs.cs
@@ -3,14 +3,29 @@ using BanchoSharp.Multiplayer;
 
 namespace BanchoSharp.EventArgs;
 
-public class PlayerChangedTeamEventArgs : System.EventArgs
+/// <summary>
+/// EventArgs for when a player changes teams in a multiplayer lobby.
+/// </summary>
+public class PlayerChangedTeamEventArgs : EventArgs
 {
-	public PlayerChangedTeamEventArgs(IMultiplayerPlayer player, TeamColor previousTeam)
-	{
-		Player = player;
-		PreviousTeam = previousTeam;
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerChangedTeamEventArgs"/> class.
+    /// </summary>
+    /// <param name="player">The player who changed teams.</param>
+    /// <param name="previousTeam">The team the player was previously on.</param>
+    public PlayerChangedTeamEventArgs(IMultiplayerPlayer player, TeamColor previousTeam)
+    {
+        Player = player;
+        PreviousTeam = previousTeam;
+    }
 
-	public IMultiplayerPlayer Player { get; }
-	public TeamColor PreviousTeam { get; }
+    /// <summary>
+    /// Gets the player who changed teams.
+    /// </summary>
+    public IMultiplayerPlayer Player { get; }
+
+    /// <summary>
+    /// Gets the team the player was previously on.
+    /// </summary>
+    public TeamColor PreviousTeam { get; }
 }
