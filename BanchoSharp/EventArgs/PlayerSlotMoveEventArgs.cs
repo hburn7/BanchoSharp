76eb355c568de182284f742cb0b28b093d32c949
hburn7 <hburnett777@gmail.com>
2023-04-14 22:52:02 -0400

    PlayerSlotMoveEventArgs docs

--- BanchoSharp/EventArgs/PlayerSlotMoveEventArgs.cs
+++ BanchoSharp/EventArgs/PlayerSlotMoveEventArgs.cs
@@ -2,16 +2,36 @@ using BanchoSharp.Interfaces;
 
 namespace BanchoSharp.EventArgs;
 
+/// <summary>
+/// Represents the event arguments for a move in player slot.
+/// </summary>
 public class PlayerSlotMoveEventArgs : System.EventArgs
 {
-	public PlayerSlotMoveEventArgs(IMultiplayerPlayer player, int previousSlot, int newSlot)
-	{
-		Player = player;
-		PreviousSlot = previousSlot;
-		NewSlot = newSlot;
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerSlotMoveEventArgs"/> class.
+    /// </summary>
+    /// <param name="player">The <see cref="IMultiplayerPlayer"/> object related to the slot move.</param>
+    /// <param name="previousSlot">The previous slot number.</param>
+    /// <param name="newSlot">The new slot number.</param>
+    public PlayerSlotMoveEventArgs(IMultiplayerPlayer player, int previousSlot, int newSlot)
+    {
+        Player = player;
+        PreviousSlot = previousSlot;
+        NewSlot = newSlot;
+    }
 
-	public IMultiplayerPlayer Player { get; }
-	public int PreviousSlot { get; }
-	public int NewSlot { get; }
+    /// <summary>
+    /// Gets the <see cref="IMultiplayerPlayer"/> object related to the slot move.
+    /// </summary>
+    public IMultiplayerPlayer Player { get; }
+
+    /// <summary>
+    /// Gets the previous slot number.
+    /// </summary>
+    public int PreviousSlot { get; }
+
+    /// <summary>
+    /// Gets the new slot number.
+    /// </summary>
+    public int NewSlot { get; }
 }
